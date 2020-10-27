05575137193fbdf70fa5e07b9e1094563d9eee42
thrwwy216 <thrwwy216@tutanota.com>
2020-10-27 07:11:41 -0500

    heroku master

--- src/app/constants/server-constant.ts
+++ src/app/constants/server-constant.ts
@@ -3,9 +3,11 @@ export class ServerConstant {
     // DEV ENV variables
     public host = 'https://spring-angular-photo-share-app.herokuapp.com';
 
+    public s3imagehost = 'https://vineyard-photos.s3.us-east-2.amazonaws.com';
+
     public client = 'https://spring-angular-photoshar-front.herokuapp.com';
 
-    public userPicture = `${this.host}/images/users`;
+    public userPicture = `${this.s3imagehost}/images/users`;
 
-    public postPicture = `${this.host}/images/posts`;
+    public postPicture = `${this.s3imagehost}/images/posts`;
 }
