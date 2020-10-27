87d3213068a3eb47a16d737ffd1753bb3c44dfe7
thrwwy216 <thrwwy216@tutanota.com>
2020-10-26 21:58:13 -0500

    Heroku

--- server.js
+++ server.js
@@ -16,3 +16,32 @@ app.get('/*', function (req, res) {
 app.listen(process.env.PORT || 8080, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+
+// app.use(express.static(path.join(__dirname, '/dist/vineyard-frontend')));
+
+
+// app.get('/json', function (req, res) {
+//     console.log("GET the json");
+//     res
+//         .status(200)
+//         .json({ "jsonData": true });
+// });
+
+// app.get('/file', function (req, res) {
+//     console.log("GET the file");
+//     res
+//         .status(200)
+//         .sendFile(path.join(__dirname, 'app.js'));
+// });
+
+
+
+// var server = app.listen(process.env.PORT || 5000), function() {
+//     var port = server.address().port;
+//     console.log("Express is working on port " + port);
+// });
