ac2e1eb28680fdc154e87a638640f504a49e7edf
thrwwy216 <thrwwy216@tutanota.com>
2020-10-26 22:40:09 -0500

    heroku

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-//require express
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+const app = express();
 
-app.listen(process.env.PORT || 8080);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/vineyard-frontend'));
 
-// Path location strategy
-
-// allows angular to handle the routing instead of the server
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+    res.sendFile(path.join(__dirname + '/dist/vineyard-frontend/index.html'));
 });
 
-console.log('Console listening!');
-
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+console.log("server listening")
 // finally add "node server.js" to our "start:" script in package.json
