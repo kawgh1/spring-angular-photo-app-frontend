eaadf8a5795e7a40cbc6d4c5551d8cfc5c218fab
thrwwy216 <thrwwy216@tutanota.com>
2020-11-03 13:48:37 -0600

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { AccountService } from '../../services/account.service';
 import { LoadingService } from '../../services/loading.service';
 import { AlertService } from '../../services/alert.service';
@@ -36,7 +36,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     public accountService: AccountService,
     private postService: PostService,
     private router: Router,
-    private route: ActivatedRoute,
     private loadingService: LoadingService,
     private alertService: AlertService
   ) { }
@@ -99,6 +98,8 @@ export class HomeComponent implements OnInit, OnDestroy {
         this.posts = response;
         console.log(this.posts);
 
+
+
         this.loadingService.isLoading.next(false);
       },
       error => {
@@ -108,22 +109,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     ));
   }
 
-  resolvePost(): void {
-    const resolvedPost: Post = this.route.snapshot.data.resolvedPost;
-    if (resolvedPost != null) {
-      console.log(resolvedPost);
-      this.post = resolvedPost;
-      this.userHost = this.postService.userHost;
-      this.postHost = this.postService.postHost;
-      this.host = this.postService.host;
-      this.getUserInfo(this.accountService.loggedInUsername);
-      this.loadingService.isLoading.next(false);
-    } else {
-      this.loadingService.isLoading.next(false);
-      this.alertService.showAlert('Post was not found.', AlertType.DANGER);
-      this.router.navigateByUrl('/home');
-    }
-  }
 
   onDelete(id: number): void {
     this.subscriptions.push(
@@ -153,29 +138,6 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   // comments and likes
-  onAddComment(comment, post: Post) {
-    this.comment.content = '';
-    const newComment: Comment = new Comment();
-    newComment.content = comment.value.content;
-    newComment.postId = comment.value.postId;
-    newComment.postedDate = new Date();
-    newComment.username = comment.value.username;
-    console.log(newComment);
-    post.commentList.push(newComment);
-    this.subscriptions.push(
-      this.postService.saveComment(newComment).subscribe(
-        response => {
-          console.log(response);
-          console.log('Comment has been saved to the database...');
-        },
-        error => {
-          this.loadingService.isLoading.next(false);
-          console.log(error);
-        }
-      )
-    );
-  }
-
   displayLike(user: User) {
     const result: Post = user.likedPost.find(post => post.id === this.post.id);
     if (result) {
@@ -188,54 +150,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
   }
 
-  likePost(post, user) {
-    if (this.color === '#000000') {
-      this.color = '#18BC9C';
-      this.like = 'Unlike';
-      this.doLike(post, user);
-      post.likes += 1;
-    } else {
-      this.color = '#000000';
-      this.like = 'Like';
-      this.doUnlike(post, user);
-      if (user.likedPosts != null) {
-        for (let i = 0; i < user.likedPosts.length; i++) {
-          if (user.likedPosts[i].id === post.id) {
-            user.likedPosts.splice(i, 1);
-          }
-        }
-      }
-      if (post.likes > 0) {
-        this.post.likes -= 1;
-      }
-    }
-  }
 
-  doLike(post, user) {
-    this.subscriptions.push(
-      this.postService.like(post.id, user.username).subscribe(
-        response => {
-          console.log(response);
-        },
-        error => {
-          console.log(error);
-        }
-      )
-    );
-  }
-
-  doUnlike(post, user) {
-    this.subscriptions.push(
-      this.postService.unLike(post.id, user.username).subscribe(
-        response => {
-          console.log(response);
-        },
-        error => {
-          console.log(error);
-        }
-      )
-    );
-  }
 
   ngOnDestroy() {
     this.subscriptions.forEach(sub => sub.unsubscribe());
